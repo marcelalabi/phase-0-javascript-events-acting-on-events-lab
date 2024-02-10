3421ec56f03589d8f0d9e914465aaebac0c7690c
Malik Alabi <marcelalabi776@gmail.com>
2024-02-10 14:27:02 -0600

    finish

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+
+    }
+});
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
